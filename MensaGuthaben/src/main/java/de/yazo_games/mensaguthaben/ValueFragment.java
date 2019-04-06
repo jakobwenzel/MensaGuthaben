c8dbab9f06af8f6af022ffbf06572f24ffef3634
quantenProjects <quantenProjects@users.noreply.github.com>
2019-04-06 20:58:11 +0200

    add Button to send last transaction to My Expenses
    https://github.com/mtotschnig/MyExpenses/blob/master/transactionscontract/src/main/java/org/totschnig/myexpenses/contract/TransactionsContract.java#

--- MensaGuthaben/src/main/java/de/yazo_games/mensaguthaben/ValueFragment.java
+++ MensaGuthaben/src/main/java/de/yazo_games/mensaguthaben/ValueFragment.java
@@ -1,12 +1,18 @@
 package de.yazo_games.mensaguthaben;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import org.totschnig.myexpenses.TransactionsContract;
 
 import java.util.Locale;
 import java.util.Currency;
@@ -18,6 +24,7 @@ public class ValueFragment extends Fragment {
 	private ValueData valueData;
 	private TextView tvCurrentValue;
 	private TextView tvLastValue;
+	private Button button;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,8 @@ public class ValueFragment extends Fragment {
 
 		tvCurrentValue = ((TextView) v.findViewById(R.id.current));
 		tvLastValue = (TextView) v.findViewById(R.id.last);
+		button = (Button) v.findViewById(R.id.button);
+
 
 		ViewCompat.setTransitionName(tvCurrentValue, "current");
 		ViewCompat.setTransitionName(tvLastValue, "last");
@@ -40,6 +49,8 @@ public class ValueFragment extends Fragment {
 		if (savedInstanceState!=null) {
 			valueData = (ValueData) savedInstanceState.getSerializable(VALUE);
 		}
+
+
 
 		updateView();
 
@@ -68,11 +79,17 @@ public class ValueFragment extends Fragment {
 				String last = moneyStr(valueData.lastTransaction);
 				tvLastValue.setText(getString(R.string.last_withdrawal) + " " + last);
 				tvLastValue.setVisibility(View.VISIBLE);
+				button.setOnClickListener( new ValueDataOnClickListener(valueData));
+				button.setVisibility(View.VISIBLE);
 			} else {
 				tvLastValue.setVisibility(View.GONE);
+				button.setVisibility(View.GONE);
 			}
 		}
 	}
+
+
+
 
 	@Override
 	public void onSaveInstanceState(Bundle bundle) {
@@ -88,5 +105,31 @@ public class ValueFragment extends Fragment {
 
 		if (tvCurrentValue !=null)
 			updateView();
+	}
+
+	public class ValueDataOnClickListener implements View.OnClickListener
+	{
+
+		ValueData valueData;
+
+		public ValueDataOnClickListener(ValueData valueData) {
+			this.valueData = valueData;
+		}
+
+		@Override
+		public void onClick(View v) {
+			Long amount = (long) (valueData.lastTransaction * -1000);
+			Intent intent = new Intent(Intent.ACTION_INSERT);
+			intent.setData(Uri.parse("content://org.totschnig.myexpenses/transactions"));
+
+			intent.putExtra(TransactionsContract.Transactions.AMOUNT_MICROS, amount);
+			intent.putExtra(TransactionsContract.Transactions.PAYEE_NAME, "Mensa");
+			intent.putExtra(TransactionsContract.Transactions.CATEGORY_LABEL, "Food:Canteen");
+			Log.i("onClick",amount.toString());
+			startActivity(intent);
+
+
+		}
+
 	}
 }
