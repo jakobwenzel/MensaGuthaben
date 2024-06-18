add7bf8dea21421fea237f98c53a07f5e0956308
castn <carsten.wesp@online.de>
2024-06-18 11:14:36 +0200

    update to sdkVersion 34

--- MensaGuthaben/src/main/java/de/yazo_games/mensaguthaben/MainActivity.java
+++ MensaGuthaben/src/main/java/de/yazo_games/mensaguthaben/MainActivity.java
@@ -129,7 +129,7 @@ public class MainActivity extends ActionBarActivity {
 		// delivering to
 		// this activity.
 		mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
-				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_IMMUTABLE);
 
 
 		// Setup an intent filter
